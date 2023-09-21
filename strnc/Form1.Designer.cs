7869f56c1cd1615ff892248e0365020569560b8e
Emre06-gazi <75805390+Emre06-gazi@users.noreply.github.com>
2023-09-21 16:28:57 +0300

    İlk
    ..

---
+++ strnc/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace strnc
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.WhitePointsLabel = new System.Windows.Forms.Label();
+            this.BlackPointsLabel = new System.Windows.Forms.Label();
+            this.CalculateButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SelectFile = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // WhitePointsLabel
+            // 
+            this.WhitePointsLabel.AutoSize = true;
+            this.WhitePointsLabel.Location = new System.Drawing.Point(50, 61);
+            this.WhitePointsLabel.Name = "WhitePointsLabel";
+            this.WhitePointsLabel.Size = new System.Drawing.Size(0, 16);
+            this.WhitePointsLabel.TabIndex = 0;
+            // 
+            // BlackPointsLabel
+            // 
+            this.BlackPointsLabel.AutoSize = true;
+            this.BlackPointsLabel.Location = new System.Drawing.Point(53, 112);
+            this.BlackPointsLabel.Name = "BlackPointsLabel";
+            this.BlackPointsLabel.Size = new System.Drawing.Size(0, 16);
+            this.BlackPointsLabel.TabIndex = 1;
+            // 
+            // CalculateButton
+            // 
+            this.CalculateButton.Location = new System.Drawing.Point(21, 30);
+            this.CalculateButton.Name = "CalculateButton";
+            this.CalculateButton.Size = new System.Drawing.Size(75, 23);
+            this.CalculateButton.TabIndex = 12;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(529, 515);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(590, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(209, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Taş Türü   -    Kısaltması   -     Puanı";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(593, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(170, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Piyon                   p                   1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(593, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(163, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "At                        a                   3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(593, 101);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(163, 16);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Fil                         f                   3";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(593, 117);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(168, 16);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Kale                     k                   5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(593, 137);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(168, 16);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Vezir                    v                   9";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(593, 157);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(176, 16);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Şah                     s                  100";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // SelectFile
+            // 
+            this.SelectFile.Location = new System.Drawing.Point(593, 228);
+            this.SelectFile.Name = "SelectFile";
+            this.SelectFile.Size = new System.Drawing.Size(246, 32);
+            this.SelectFile.TabIndex = 11;
+            this.SelectFile.Text = "Dosya Seç ve Puanını Hesapla";
+            this.SelectFile.UseVisualStyleBackColor = true;
+            this.SelectFile.Click += new System.EventHandler(this.SelectFile_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(890, 535);
+            this.Controls.Add(this.SelectFile);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.CalculateButton);
+            this.Controls.Add(this.BlackPointsLabel);
+            this.Controls.Add(this.WhitePointsLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label WhitePointsLabel;
+        private System.Windows.Forms.Label BlackPointsLabel;
+        private System.Windows.Forms.Button CalculateButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button SelectFile;
+    }
+}
+
---
+++ strnc/Form1.cs
@@ -0,0 +1,448 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace strnc
+{
+    public partial class Form1 : Form
+    {
+        private const int TileSize = 50; // Tahta üzerindeki her karenin boyutu (piksel cinsinden)
+
+        private readonly Chessboard chessboard; // Satranç tahtasını temsil eden nesne
+
+        public Form1()
+        {
+            InitializeComponent();
+            chessboard = new Chessboard(); // Satranç tahtasını oluştur
+        }
+
+        private void DrawChessboard(List<ChessPiece> chessPieces)
+        {
+            // Satranç tahtasını çizmek için bir bitmap oluştur
+            Bitmap boardBitmap = new Bitmap(TileSize * 8, TileSize * 8);
+            Graphics g = Graphics.FromImage(boardBitmap);
+
+            // Tahtayı arka plan rengi ile doldur
+            g.FillRectangle(Brushes.BurlyWood, 0, 0, boardBitmap.Width, boardBitmap.Height);
+
+            for (int row = 0; row < 8; row++)
+            {
+                for (int col = 0; col < 8; col++)
+                {
+                    // Karelerin rengini belirle
+                    Color tileColor = (row + col) % 2 == 0 ? Color.Cyan : Color.DarkCyan;
+                    Brush tileBrush = new SolidBrush(tileColor);
+
+                    // Kareyi çiz
+                    g.FillRectangle(tileBrush, col * TileSize, row * TileSize, TileSize, TileSize);
+
+                    // Karede taş varsa
+                    ChessPiece piece = chessPieces.FirstOrDefault(p => p.Row == row && p.Col == col);
+                    if (piece != null)
+                    {
+                        // Taşın tehdit edilip edilmediğini kontrol et
+                        bool isThreatened = chessboard.IsPieceThreatened(piece, chessPieces);
+                        Color pieceTileColor = isThreatened ? Color.Red : tileColor;
+
+                        // Taşın rengini belirle ve kareyi çiz
+                        g.FillRectangle(new SolidBrush(pieceTileColor), col * TileSize, row * TileSize, TileSize, TileSize);
+
+                        // Taşın tipini ve koordinatlarını yazdır
+                        Brush pieceBrush = piece.Color == ChessPieceColor.Beyaz ? Brushes.White : Brushes.Black;
+                        Font boldFont = new Font("Arial", 11, FontStyle.Bold);
+                        g.DrawString(piece.Type.ToString(), boldFont, pieceBrush, col * TileSize, row * TileSize);
+                        string coordinates = $"{chessboard.GetColumnLetter(col)}{8 - row}";
+                        g.DrawString(coordinates, new Font("Arial", 11), Brushes.Black, col * TileSize, row * TileSize + 30);
+                    }
+                }
+            }
+
+            // PictureBox'a tahtayı yerleştir
+            pictureBox1.Image = boardBitmap;
+        }
+
+        private void ShowCalculationProcess(List<ChessPiece> chessPieces, ChessPieceColor color, double points)
+        {
+            // Hesaplama sürecini kullanıcıya gösteren bir iletişim penceresi oluştur
+            string message = $"{(color == ChessPieceColor.Siyah ? "Siyah" : "Beyaz")} Taşların Hesaplanma Süreci:\n";
+
+            foreach (ChessPiece piece in chessPieces)
+            {
+                if (piece.Color == color)
+                {
+                    // Taşın tehdit edilip edilmediğini kontrol et ve puan hesapla
+                    double threatenedMultiplier = chessboard.IsPieceThreatened(piece, chessPieces) ? 0.5 : 1.0;
+                    double piecePoints = piece.GetPoints() * threatenedMultiplier;
+                    string coordinates = $"{chessboard.GetColumnLetter(piece.Col)}{8 - piece.Row}";
+
+                    // Hesaplama sonuçlarını iletişim penceresine ekle
+                    message += $"{piece.Color} {piece.Type} ({coordinates}): {piece.GetPoints()} x {threatenedMultiplier} = {piecePoints}\n";
+                }
+            }
+
+            // Toplam puanı iletişim penceresine ekleyin
+            message += $"\nToplam Puan: {points}";
+
+            MessageBox.Show(message, "Hesaplama Sonucu", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        // Dosya seçme düğmesine tıklandığında çalışacak metod
+        private void SelectFile_Click(object sender, EventArgs e)
+        {
+            // Dosya seçme işlemi için bir OpenFileDialog oluştur
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Metin Dosyaları (*.txt)|*.txt";
+            openFileDialog.FileName = "chessboard.txt"; // Varsayılan dosya adı
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Kullanıcı bir dosya seçtiğinde, seçilen dosyanın yolunu al
+                string secilenDosyaYolu = openFileDialog.FileName;
+
+                try
+                {
+                    // Dosyayı oku ve işle
+                    List<string> satirlar = File.ReadAllLines(secilenDosyaYolu).ToList();
+
+                    if (satirlar.Count != 8)
+                    {
+                        MessageBox.Show("Dosya, 8 satır içermelidir.", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+
+                    List<ChessPiece> chessPieces = chessboard.ParseChessPieces(satirlar);
+
+                    if (chessPieces.Count > 32 || chessPieces.Count <= 2)
+                    {
+                        MessageBox.Show("Dosya geçerli bir oyun durumu içermelidir (32 taş veya daha az taş).", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+
+                    // Satranç tahtasını çiz
+                    DrawChessboard(chessPieces);
+
+                    // Beyaz ve siyah taşların puanlarını hesapla ve göster
+                    double whitePoints = chessboard.CalculatePoints(chessPieces, ChessPieceColor.Beyaz);
+                    double blackPoints = chessboard.CalculatePoints(chessPieces, ChessPieceColor.Siyah);
+
+                    ShowCalculationProcess(chessPieces, ChessPieceColor.Beyaz, whitePoints);
+                    ShowCalculationProcess(chessPieces, ChessPieceColor.Siyah, blackPoints);
+                }
+                catch (FileNotFoundException)
+                {
+                    MessageBox.Show("Dosya bulunamadı.", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                catch (IOException)
+                {
+                    MessageBox.Show("Dosya okunamadı.", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Beklenmeyen bir hata oluştu: " + ex.Message, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+    }
+
+    // Satranç taşlarının türlerini temsil eden enum
+    public enum ChessPieceType
+    {
+        Piyon = 'p',
+        At = 'a',
+        Fil = 'f',
+        Kale = 'k',
+        Vezir = 'v',
+        Şah = 's'
+    }
+
+    // Satranç taşlarının renklerini temsil eden enum
+    public enum ChessPieceColor
+    {
+        Beyaz = 's',
+        Siyah = 'b'
+    }
+
+    // Satranç taşlarını temsil eden sınıf
+    public class ChessPiece
+    {
+        public ChessPieceType Type { get; } // Taşın türü
+        public ChessPieceColor Color { get; } // Taşın rengi
+        public double Points { get; } // Taşın puan değeri
+        public int Row { get; } // Taşın satır konumu
+        public int Col { get; } // Taşın sütun konumu
+
+        public ChessPiece(ChessPieceType type, ChessPieceColor color, int row, int col)
+        {
+            Type = type;
+            Color = color;
+            Points = GetPiecePoints(type);
+            Row = row;
+            Col = col;
+        }
+
+        public double GetPoints()
+        {
+            return Points;
+        }
+
+        // Taşın türüne göre puanını döndüren metod
+        public static double GetPiecePoints(ChessPieceType type)
+        {
+            switch (type)
+            {
+                case ChessPieceType.Piyon:
+                    return 1.0;
+                case ChessPieceType.At:
+                    return 3.0;
+                case ChessPieceType.Fil:
+                    return 3.0;
+                case ChessPieceType.Kale:
+                    return 5.0;
+                case ChessPieceType.Vezir:
+                    return 9.0;
+                case ChessPieceType.Şah:
+                    return 100.0;
+                default:
+                    return 0.0;
+            }
+        }
+    }
+
+    // Satranç tahtasını temsil eden sınıf
+    public class Chessboard
+    {
+        // Dosyadan okunan satırları satranç taşlarına dönüştüren metod
+        public List<ChessPiece> ParseChessPieces(List<string> lines)
+        {
+            List<ChessPiece> chessPieces = new List<ChessPiece>();
+
+            for (int row = 0; row < lines.Count; row++)
+            {
+                string[] pieces = lines[row].Split(' ');
+
+                for (int col = 0; col < pieces.Length; col++)
+                {
+                    string piece = pieces[col];
+                    if (piece != "--")
+                    {
+                        ChessPieceType type = (ChessPieceType)piece[0];
+                        ChessPieceColor color = (ChessPieceColor)piece[1];
+                        ChessPiece chessPiece = new ChessPiece(type, color, row, col);
+                        chessPieces.Add(chessPiece);
+                    }
+                }
+            }
+
+            return chessPieces;
+        }
+
+        // Belirli bir renkteki taşların toplam puanını hesaplayan metod
+        public double CalculatePoints(List<ChessPiece> chessPieces, ChessPieceColor color)
+        {
+            double points = 0;
+
+            foreach (ChessPiece piece in chessPieces)
+            {
+                if (piece.Color == color)
+                {
+                    // Taşın tehdit edilip edilmediğini kontrol et
+                    bool isThreatened = IsPieceThreatened(piece, chessPieces);
+                    double threatenedMultiplier = isThreatened ? 0.5 : 1.0;
+                    double piecePoints = ChessPiece.GetPiecePoints(piece.Type) * threatenedMultiplier;
+                    points += piecePoints;
+                }
+            }
+
+            return points;
+        }
+
+        // Bir taşın tehdit edilip edilmediğini kontrol eden metod
+        public bool IsPieceThreatened(ChessPiece piece, List<ChessPiece> chessPieces)
+        {
+            foreach (ChessPiece opponent in chessPieces)
+            {
+                if (opponent.Color != piece.Color)
+                {
+                    if (IsPieceThreatenedByOpponent(opponent, piece, chessPieces))
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        // Bir taşın belirli bir rakip tarafından tehdit edilip edilmediğini kontrol eden metod
+        public bool IsPieceThreatenedByOpponent(ChessPiece piece, ChessPiece opponent, List<ChessPiece> chessPieces)
+        {
+            switch (piece.Type)
+            {
+                case ChessPieceType.Piyon:
+                    return IsPawnThreatenedByOpponent(piece, opponent);
+                case ChessPieceType.At:
+                    return IsKnightThreatenedByOpponent(piece, opponent);
+                case ChessPieceType.Fil:
+                    return IsBishopThreatenedByOpponent(piece, opponent, chessPieces);
+                case ChessPieceType.Kale:
+                    return IsRookThreatenedByOpponent(piece, opponent, chessPieces);
+                case ChessPieceType.Vezir:
+                    return IsQueenThreatenedByOpponent(piece, opponent, chessPieces);
+                case ChessPieceType.Şah:
+                    return IsKingThreatenedByOpponent(piece, opponent);
+                default:
+                    return false;
+            }
+        }
+
+        // Bir piyonun belirli bir rakip tarafından tehdit edilip edilmediğini kontrol eden metod
+        public bool IsPawnThreatenedByOpponent(ChessPiece pawn, ChessPiece opponent)
+        {
+            int rowDiff = opponent.Row - pawn.Row;
+            int colDiff = Math.Abs(opponent.Col - pawn.Col);
+
+            if (pawn.Color == ChessPieceColor.Beyaz)
+            {
+                if (rowDiff == 1 && colDiff == 1 && opponent.Row > pawn.Row)
+                    return true;
+            }
+            else if (pawn.Color == ChessPieceColor.Siyah)
+            {
+                if (rowDiff == -1 && colDiff == 1 && opponent.Row < pawn.Row)
+                    return true;
+            }
+
+            return false;
+        }
+
+        // Bir atın belirli bir rakip tarafından tehdit edilip edilmediğini kontrol eden metod
+        public bool IsKnightThreatenedByOpponent(ChessPiece knight, ChessPiece opponent)
+        {
+            int rowDiff = Math.Abs(knight.Row - opponent.Row);
+            int colDiff = Math.Abs(knight.Col - opponent.Col);
+            return (rowDiff == 2 && colDiff == 1) || (rowDiff == 1 && colDiff == 2);
+        }
+
+        // Bir filin belirli bir rakip tarafından tehdit edilip edilmediğini kontrol eden metod
+        public bool IsBishopThreatenedByOpponent(ChessPiece bishop, ChessPiece opponent, List<ChessPiece> chessPieces)
+        {
+            int rowDiff = Math.Abs(bishop.Row - opponent.Row);
+            int colDiff = Math.Abs(bishop.Col - opponent.Col);
+
+            if (rowDiff == colDiff)
+            {
+                int rowStep = Math.Sign(opponent.Row - bishop.Row);
+                int colStep = Math.Sign(opponent.Col - bishop.Col);
+
+                int currentRow = bishop.Row + rowStep;
+                int currentCol = bishop.Col + colStep;
+
+                while (currentRow != opponent.Row || currentCol != opponent.Col)
+                {
+                    if (chessPieces.Any(p => p.Row == currentRow && p.Col == currentCol))
+                    {
+                        return false;
+                    }
+
+                    currentRow += rowStep;
+                    currentCol += colStep;
+                }
+
+                return true;
+            }
+
+            return false;
+        }
+
+        // Bir kalin belirli bir rakip tarafından tehdit edilip edilmediğini kontrol eden metod
+        public bool IsRookThreatenedByOpponent(ChessPiece rook, ChessPiece opponent, List<ChessPiece> chessPieces)
+        {
+            if (rook.Row == opponent.Row || rook.Col == opponent.Col)
+            {
+                int minRow = Math.Min(rook.Row, opponent.Row);
+                int maxRow = Math.Max(rook.Row, opponent.Row);
+                int minCol = Math.Min(rook.Col, opponent.Col);
+                int maxCol = Math.Max(rook.Col, opponent.Col);
+
+                bool blockingPieceFound = false;
+
+                if (rook.Row == opponent.Row)
+                {
+                    for (int col = minCol + 1; col < maxCol; col++)
+                    {
+                        ChessPiece blockingPiece = chessPieces.FirstOrDefault(p => p.Row == rook.Row && p.Col == col);
+                        if (blockingPiece != null)
+                        {
+                            blockingPieceFound = true;
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    for (int row = minRow + 1; row < maxRow; row++)
+                    {
+                        ChessPiece blockingPiece = chessPieces.FirstOrDefault(p => p.Row == row && p.Col == rook.Col);
+                        if (blockingPiece != null)
+                        {
+                            blockingPieceFound = true;
+                            break;
+                        }
+                    }
+                }
+
+                return !blockingPieceFound;
+            }
+
+            return false;
+        }
+
+        // Bir vezirin belirli bir rakip tarafından tehdit edilip edilmediğini kontrol eden metod
+        public bool IsQueenThreatenedByOpponent(ChessPiece queen, ChessPiece opponent, List<ChessPiece> chessPieces)
+        {
+            int rowDiff = Math.Abs(queen.Row - opponent.Row);
+            int colDiff = Math.Abs(queen.Col - opponent.Col);
+
+            if (rowDiff == colDiff || queen.Row == opponent.Row || queen.Col == opponent.Col)
+            {
+                int rowStep = Math.Sign(opponent.Row - queen.Row);
+                int colStep = Math.Sign(opponent.Col - queen.Col);
+
+                int currentRow = queen.Row + rowStep;
+                int currentCol = queen.Col + colStep;
+
+                while (currentRow != opponent.Row || currentCol != opponent.Col)
+                {
+                    if (chessPieces.Any(p => p.Row == currentRow && p.Col == currentCol))
+                    {
+                        return false;
+                    }
+
+                    currentRow += rowStep;
+                    currentCol += colStep;
+                }
+
+                return true;
+            }
+
+            return false;
+        }
+
+        // Bir şahın belirli bir rakip tarafından tehdit edilip edilmediğini kontrol eden metod
+        public bool IsKingThreatenedByOpponent(ChessPiece king, ChessPiece opponent)
+        {
+            int rowDiff = Math.Abs(king.Row - opponent.Row);
+            int colDiff = Math.Abs(king.Col - opponent.Col);
+            return rowDiff <= 1 && colDiff <= 1;
+        }
+
+        // Sütun numarasını harf olarak döndüren metod
+        public char GetColumnLetter(int col)
+        {
+            return (char)('A' + col);
+        }
+    }
+}
